b7328ed166098fa9603cf585778bd2e361a1f5bc
Sofaxw <sofia.jaraa@usm.cl>
2024-05-25 00:27:38 -0400

    a
    a

--- core/views.py
+++ core/views.py
@@ -6,10 +6,9 @@ import time
 
 
 # Create your views here.
-# Define a constant for the maximum number of login attempts
 MAX_LOGIN_ATTEMPTS = 3
-# Define a constant for the lockout duration in seconds
-LOCKOUT_DURATION = 180  # 5 minutes
+
+LOCKOUT_DURATION = 180  # 5 minutos
 
 def base(request):
     return render(request, 'base.html')
@@ -23,7 +22,6 @@ def loginview(request):
         username = request.POST['username']
         password = request.POST['password']
         
-        # Check if the user is currently locked out
         lockout_key = f"login_lockout_{username}"
         if cache.get(lockout_key):
             return render(request, 'login.html',{
@@ -34,12 +32,10 @@ def loginview(request):
         user = authenticate(request, username=username, password=password)
         attempts_key = f"login_attempts_{username}"
         if user is None:
-            # Increment the login attempts counter
             attempts = cache.get(attempts_key, 0) + 1
             cache.set(attempts_key, attempts, timeout=LOCKOUT_DURATION)
 
             if attempts >= MAX_LOGIN_ATTEMPTS:
-                # If maximum attempts reached, lock the account
                 cache.set(lockout_key, True, timeout=LOCKOUT_DURATION)
                 return render(request, 'login.html',{
                     'form': AuthenticationForm(), 
@@ -52,7 +48,6 @@ def loginview(request):
                 })
         
         else:
-            # If login successful, reset the login attempts counter
             cache.delete(attempts_key)
             login(request, user)
             return redirect('base')
