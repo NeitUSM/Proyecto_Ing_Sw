f73f3e31532444e0ebb29fe448ea188ade3ca671
Sofaxw <sofia.jaraa@usm.cl>
2024-05-24 22:49:39 -0400

    login

--- core/views.py
+++ core/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-
 # Create your views here.
 
-@login_required
-def home(request):
-    return render(request, 'core/home.html')
 
+def base(request):
+    return render(request, 'base.html')
+
+def loginview(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'form': AuthenticationForm()
+        })
+    else:
+        user= authenticate(request, username= request.POST['username'], password= request.POST['password'])
+        
+        if user is None:
+            return render(request, 'login.html',{
+                "form": AuthenticationForm(), 
+                "error":'El correo o contraseña es incorrecto'
+            })
+        
+        else:
+            login(request, user)
+            return redirect('base')
+
+        
+
+    
--- ing_sw_project/urls.py
+++ ing_sw_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
-    path('', include('core.urls') ),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.loginview, name='login'),
+    path('base/', views.base, name='base'),
 ]
