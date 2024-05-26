d96a72e0583b3088be17ea742734c255b843f53c
MaikAF <miguelalfaro1202@gmail.com>
2024-05-25 20:10:29 -0400

    Criterios de aceptacion listos
    Criterios de aceptacion terminados

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .form import CreateUserForm
 
-# Create your views here.
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cuenta creada exitosamente!')
+            return redirect('exito') 
+        else:
+            messages.error(request, 'Por favor corrige los errores.')
+    context = {'form': form}
+    return render(request, 'core/register.html', context)
+
+def exito(request):
+    return render(request, 'core/exito.html')
+
+def login(request):
+    return render(request, 'core/login.html')
--- ing_sw_project/settings.py
+++ ing_sw_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'ing_sw_project.wsgi.application'
 
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
