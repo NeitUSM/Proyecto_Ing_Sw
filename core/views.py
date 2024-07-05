148588e18c5e0694d4624dd9e380ca3f197940db
NeitUSM <natan.apablaza@usm.cl>
2024-07-04 22:27:27 -0400

    Boton comparar en navbar

--- core/views.py
+++ core/views.py
@@ -1,24 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Carrera
+from django.http import JsonResponse
 
 def listar_carreras(request):
     carreras = Carrera.objects.all()
     return render(request, 'core/lista_carreras.html', {'carreras': carreras})
 
 def agregar_a_comparacion(request, carrera_id):
-    print(f"Request recibido para agregar carrera ID: {carrera_id}")
-    
-    carrera = get_object_or_404(Carrera, IdCarrera=carrera_id)
-    print(f"Carrera encontrada: {carrera.NombreCarrera}")
-
+    # Verificar si la carrera con el ID especificado existe
+    get_object_or_404(Carrera, IdCarrera=carrera_id)
     comparacion = request.session.get('comparacion', [])
     if len(comparacion) < 2 and carrera_id not in comparacion:
         comparacion.append(carrera_id)
         request.session['comparacion'] = comparacion
-
-    print("Comparación actual:", request.session['comparacion'])
-    
-    return redirect('lista_carreras')
+    return JsonResponse({'success': True, 'comparacion_count': len(comparacion)})
 
 def eliminar_de_comparacion(request, carrera_id):
     comparacion = request.session.get('comparacion', [])
