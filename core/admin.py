33a0d74684f2519ed1f67614165d0e25daa56bfd
ferABS <141463832+ferABS@users.noreply.github.com>
2024-07-08 23:13:31 -0400

    Revert "Alonso"

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Carrera, Comparacion
 
-# Registra tus modelos aquí
-admin.site.register(Carrera)
-admin.site.register(Comparacion)
+# Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
-class Carrera(models.Model):
-    IdCarrera = models.AutoField(primary_key=True)
-    NombreUniversidad = models.CharField(max_length=255)
-    NombreCarrera = models.CharField(max_length=255)
-    DescripcionCarrera = models.TextField()
-    SemestresCarrera = models.PositiveIntegerField()
-    RegionCarrera = models.PositiveIntegerField()
-    ComunaCarrera = models.CharField(max_length=255)
-    AreaEstudio = models.CharField(max_length=100)
-    Arancel = models.PositiveIntegerField(default=0)  # Define un valor predeterminado
-    Matricula = models.PositiveIntegerField(default=0)  # Define un valor predeterminado
-    Gratuidad = models.BooleanField(default=False)  # Define un valor predeterminado
-    Regimen = models.CharField(max_length=50, default='Diurno')  # Define un valor predeterminado
-    link = models.URLField(max_length=200, default='')  # Define un valor predeterminado
-    
-    def __str__(self):
-        return f"{self.NombreCarrera} - {self.NombreUniversidad}"
-
-class Comparacion(models.Model):
-    carreras = models.ManyToManyField(Carrera)
+# Create your models here.
